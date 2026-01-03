cc8469de66166a36cb23319a3b19549bcb3984bc
AhsanHafeez-dev <ahsanhafeez724@gmail.com>
2026-01-03 12:42:01 +0500

    react learning

---
+++ backgroundChanger/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  
+  return (
+    <>
+      <div className=" bg-yellow-500 p-10 m-25 border-2 h-[200px] box-border"> I am a box</div>
+      <div className="bg-yellow-500 p-10 m-25 border-2 border-black h-[200px] box-border"> I am a box</div>
+
+    </>
+  )
+}
+
+export default App
