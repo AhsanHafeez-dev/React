914ccedce8fd7a47712d2eb1c3c0f83f4c7c1c3c
AhsanHafeez-dev <ahsanhafeez724@gmail.com>
2026-01-05 16:55:07 +0500

    react learning

---
+++ props/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Card from './components/Card.jsx';
+function App() {
+  
+
+
+  return (
+    <>
+    <Card userName='ahsan' btnText='newbtn' />
+    <Card/>
+    </>
+   
+  )
+}
+
+export default App
