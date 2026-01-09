1801b01a5ebf07e4fbae7e8881e7edfda6756f33
AhsanHafeez-dev <ahsanhafeez724@gmail.com>
2026-01-09 11:57:51 +0500

    react learning

--- myOwnPractice/src/App.jsx
+++ myOwnPractice/src/App.jsx
@@ -1,21 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useEffect, useState } from 'react'
 import './App.css'
-import Header from './components/header/Header'
-import Home from './components/home/Home'
-import Footer from './components/footer/Footer'
+import { ThememContextProvider } from "./contexts/ThemeContext"
+import ThemeBtn from './components/ThemeBtn';
+import Card from './components/Card';
+
+
 
 function App() {
   
 
+  const [themeMode, setThemeMode] = useState("light");
+  const lightTheme = () => {
+    setThemeMode("light");
+  }
+  
+  const darkTheme = () => {
+    setThemeMode("dark");
+  }
+
+
+  useEffect(
+    () => { 
+      document.querySelector('html').classList.remove('light', 'dark');
+      document.querySelector('html').classList.add(themeMode);
+
+    },
+    [themeMode])
   return (
-    <>
-      <h1>hkjkjkjdkjd</h1>
-      {/* <Header />
-      <Home />
-      <Footer/> */}
-    </>
+    <ThememContextProvider value={{themeMode,lightTheme,darkTheme}}>
+
+    <div className="flex flex-wrap min-h-screen items-center">
+          <div className="w-full">
+              <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                <ThemeBtn/>    
+              </div>
+
+              <div className="w-full max-w-sm mx-auto">
+                  <Card/>
+              </div>
+          </div>
+      </div>
+      </ThememContextProvider>
+
+      
+      
   )
 }
 
