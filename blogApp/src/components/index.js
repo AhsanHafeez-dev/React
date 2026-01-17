181b8c920013588dab996ea3d20f75166f9c1646
AhsanHafeez-dev <ahsanhafeez724@gmail.com>
2026-01-17 08:34:25 +0500

    learning react

---
+++ blogApp/src/components/index.js
@@ -0,0 +1,33 @@
+import Footer from "./footer/Footer";
+import Header from "./header/Header";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./header/LogputBtn";
+import Input from './Input';
+import PostCard from './PostCard'
+import Select from './Select';
+
+import RTE from "./RTE";
+import Login from './Login';
+import AuthLayout from './AuthLayout'
+import Button from "./Button";
+
+import PostForm from './post-form/PostForm'
+import Signup from './Signup'
+export {
+    Footer,
+    Header,
+    Container,
+    Logo,
+    LogoutBtn,
+    Input,
+    PostCard,
+    Select,
+    Button,
+    RTE,
+    Login,
+    AuthLayout,
+    PostForm,
+    Signup
+
+};
