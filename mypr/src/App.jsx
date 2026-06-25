0156755a75474be45ce88f5987220d65e5759e1a
AhsanHafeez-dev <ahsanhafeez724@gmail.com>
2026-06-25 11:03:08 +0500

    practice project

---
+++ mypr/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/header/Header.jsx";
+import Home from "./components/home/Home.jsx";
+import Footer from "./components/footer/Footer.jsx";
+
+function App() {
+
+
+  return (
+    <>
+      <Header/>
+      <Home />
+      <Footer/>
+    </>
+  )
+}
+
+export default App
